bda04833064d28817d8b451b43ce0bc4fb20ae30
Stiv <strodriguezna@gmail.com>
2023-10-19 10:11:03 +0200

    Dando un color al username cuando se conecta al CHAT

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -32,9 +32,16 @@ export class ChatComponent implements OnInit{
       this.client.subscribe('/chat/mensaje', e => {
         let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
         mensaje.fecha = new Date(mensaje.fecha);
+
+        if (!this.mensaje.color && mensaje.tipo == 'NUEVO_USUARIO' && this.mensaje.username == mensaje.username) {
+          this.mensaje.color = mensaje.color;
+        }
+
         this.mensajes.push(mensaje);
         console.log(mensaje)
+
       });
+
       this.mensaje.tipo = 'NUEVO_USUARIO';
       this.client.publish({destination:'/app/mensaje', body: JSON.stringify(this.mensaje)});
 
